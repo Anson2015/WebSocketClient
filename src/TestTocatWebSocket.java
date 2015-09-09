70270050e47effa4ff90c42c8f74d8d6e50380bd
Anson2015 <297856005@qq.com>
2015-09-09 23:06:28 +0800

    websocketClient

---
+++ src/TestTocatWebSocket.java
@@ -0,0 +1,48 @@
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Scanner;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft_17;
+import org.java_websocket.handshake.ServerHandshake;
+
+public class TestTocatWebSocket {
+	public static void main(String[] args) throws URISyntaxException {
+
+		String url = "ws://localhost:8080/WebSocket/chat/demo";
+		WebSocketClient wc = new WebSocketClient(new URI(url), new Draft_17()) {
+			@Override
+			public void onOpen(ServerHandshake handshakedata) {
+				System.out.println(handshakedata.getHttpStatusMessage());
+			}
+
+			@Override
+			public void onMessage(String message) {
+				System.out.println(message);
+			}
+
+			@Override
+			public void onError(Exception ex) {
+
+			}
+
+			@Override
+			public void onClose(int code, String reason, boolean remote) {
+
+			}
+		};
+
+		wc.connect();
+		Scanner scanner = new Scanner(System.in);
+		while (true) {
+			if(scanner.hasNext()){
+				String message = scanner.nextLine();
+				if (message.equals("q")) {
+					wc.close();
+					break;
+				}
+				wc.send(message);
+			}
+		}
+	}
+}
